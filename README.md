e7a864ec7a36f57f3deb877b35afafe974688a19
mariiakhairi <mariia_khairi@inbox.ru>
2023-08-11 22:52:21 +0800

    new

--- README.md
+++
@@ -1,4 +0,0 @@
-# Fun
- 1. install VS code
- 2. add ritwickdey.LiveServer app
- 3. run the code
--- index.js
+++
@@ -1,6 +0,0 @@
-// Draw some text to the screen:
-// drawName("Ready to Code?");
-// drawName("Mariia Khairi");
-drawName("Coding is fun");
-// Animate the text!
-bounceBubbles();
